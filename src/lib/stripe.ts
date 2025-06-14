0341b3fe572ed8a55b0bd13adb7b6697452ce047
erickkkyt <kh844257437@gmail.com>
2025-06-14 15:57:50 +0800

    0614-v10

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10', // 使用最新的 API 版本
+  apiVersion: '2025-05-28.basil', // 使用最新的 API 版本
   typescript: true,
-}); 
+});
